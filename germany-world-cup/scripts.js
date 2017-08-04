332cc9b22d21432a8e3545a260358748fbaf0f53
ChrisCioffi <cjcioffi@umd.edu>
2017-08-04 12:13:40 -0400

    from yester
    day

---
+++ germany-world-cup/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+$("#legendhide").click(function() {
+$(".overlaybox").toggle();
+$("#show").toggle();
+$("#hidden").toggle();
+$("#legendhide").toggleClass("btn-primary btn-danger");
+
+});
+  });
