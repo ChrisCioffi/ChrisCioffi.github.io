bee2eaf3041558c08a4f64a5947d485dde605081
ChrisCioffi <cjcioffi@umd.edu>
2017-08-14 14:00:01 -0400

    hopefully
    working

--- final-project/js/app.js
+++ final-project/js/app.js
@@ -76,7 +76,7 @@ var line = d3.svg.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.users); });
 
-var svg = d3.select(".linechart").append("svg")
+var svg = d3.select("#linechart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
