ce49540ebea697b23136b7d94c11fd3830b8697d
ChrisCioffi <cjcioffi@umd.edu>
2017-08-14 13:58:45 -0400

    went
    back to what worked

--- final-project/js/app.js
+++ final-project/js/app.js
@@ -106,7 +106,7 @@ d3.tsv("data.tsv", function(error, data) {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Percentage of dentists");
+      .text("Percentage of dentists accepting Medicaid");
 
   svg.append("path")
       .datum(data)
